07cc63826d7e1417bcc9548c00e42802ea1dc3cf
mkiscore-marketing <mk_marketing@mkiscore.com>
2026-08-31 21:08:04 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Google Apps Script를 배포한 뒤 발급되는 웹앱 URL을 아래에 붙여넣어 주세요.
 // 설정 방법은 README.md의 "Google Apps Script 배포" 절을 참고하세요.
 window.MKI_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx1xB3L0O3thfcO9d1LsisCRyi06RKq6yb9hO7rYj3fmeCwQnvp7nWRUFesewarkvtT9Q/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzoG7OkDu38-UW7I4IuOlWMxlwRajXjGqljacv_sR3K78v0NMzcma84lHu3mqSYPqWF8w/exec",
   SHEET_URL: "https://docs.google.com/spreadsheets/d/1fd-sOFZB96wRbtXSKTM16sJ1Gc5s-OkrrLcL7Z8Rpu4/edit"
 };
